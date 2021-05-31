5306de47522925e844e809b2064d355bd7e46c89
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-05-31 10:05:44 +0200

    More halogen

--- src/Main.purs
+++ src/Main.purs
@@ -14,88 +14,110 @@ import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 import LZString (compressToEncodedURIComponent, decompressFromEncodedURIComponent)
 import LocationString (getFragmentString, setFragmentString)
-import Web.DOM (NonElementParentNode)
-import Web.DOM.Document (toNonElementParentNode)
-import Web.DOM.Element (Element)
-import Web.DOM.NonElementParentNode (getElementById)
-import Web.HTML (window)
-import Web.HTML.HTMLDocument (toDocument)
-import Web.HTML.HTMLTextAreaElement (HTMLTextAreaElement)
-import Web.HTML.HTMLTextAreaElement as HTMLTextAreaElement
-import Web.HTML.Window (document)
+import Web.HTML as WH
+import Web.HTML.HTMLTextAreaElement as WH.HTMLTextAreaElement
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  H.liftEffect loadTablatureFromFragment
   runUI component unit body
 
-data State = ViewMode | EditMode
+data Mode = ViewMode | EditMode
+type State = { mode :: Mode, tablature :: String}
 
-data Action = Save
+
+data Action = Initialize | ToggleMode
+
+refTablatureEditor :: H.RefLabel
+refTablatureEditor = H.RefLabel "tablatureEditor"
 
 component :: forall query input output m. MonadEffect m => H.Component query input output m
 component =
   H.mkComponent
     { initialState
     , render
-    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+    , eval: H.mkEval H.defaultEval 
+      { handleAction = handleAction 
+      , initialize = Just Initialize
+      }
     }
 
 initialState :: forall input. input -> State
-initialState _ = ViewMode
+initialState _ = { mode: EditMode, tablature: "" }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state =
   HH.div_
     [ renderHeader
-    , renderButton
-    , renderTextarea
+    , renderControls
+    , renderEditor
+    , renderViewer
     ]
-
-renderHeader = HH.h1_ [HH.text "Dozenal Tablature Viewer"]
-renderButton = HH.button [ HE.onClick \_ -> Save ] [ HH.text "Save" ]
-renderTextarea = HH.textarea [ HP.classes [HH.ClassName "textinput"], HP.placeholder "Paste your plaintext tablature here" ]
+  where
+  renderHeader = HH.h1_ [ HH.text "Dozenal Tablature Viewer" ]
+  renderControls = HH.button [ HE.onClick \_ -> ToggleMode ] [ HH.text "ToggleMode" ]
+  renderEditor = HH.textarea 
+    [ HP.classes
+      [ HH.ClassName "tablatureText"
+      , HH.ClassName "tablatureEditor"
+      ]
+    , HP.ref refTablatureEditor
+    , HP.placeholder "Paste your plaintext tablature here"
+    ]
+  renderViewer = HH.p [ HP.classes [HH.ClassName "tablatureText"] ] [ HH.text state.tablature ]
 
 handleAction :: forall output m. MonadEffect m => Action -> H.HalogenM State Action () output m Unit
-handleAction = case _ of
-  Save -> H.liftEffect saveTablatureToFragment
+handleAction action =
+  case action of
+    Initialize -> do
+      string <- H.liftEffect getTablatureTextFromFragment
+      state <- H.get
+      H.put { mode: state.mode, tablature: string }
+      setTablatureEditorText string
+    ToggleMode -> do
+      state <- H.get
+      case state.mode of
+        EditMode -> do
+          H.put { mode: ViewMode, tablature: state.tablature }
+          saveTablature
+        ViewMode -> do
+          H.put { mode: EditMode, tablature: state.tablature }
 
-saveTablatureToFragment :: Effect Unit
-saveTablatureToFragment = do
-  string <- getTablatureText
-  case compressToEncodedURIComponent string of
-    Just compressed -> setFragmentString compressed
-    Nothing -> Console.error("Could not save tablature to URL")
+getTablatureEditorElement :: forall output m. H.HalogenM State Action () output m (Maybe WH.HTMLTextAreaElement)
+getTablatureEditorElement = H.getHTMLElementRef refTablatureEditor <#>
+  \maybeElement -> maybeElement >>= WH.HTMLTextAreaElement.fromHTMLElement
 
-loadTablatureFromFragment :: Effect Unit
-loadTablatureFromFragment = do
-  string <- getFragmentString
-  case decompressFromEncodedURIComponent string of
-    Just decompressed -> setTablatureText decompressed
-    Nothing -> Console.error("Could not load tablature from URL")
-
-getDocument :: Effect NonElementParentNode
-getDocument = window >>= document <#> toDocument <#> toNonElementParentNode
-
-documentGetElementById :: String -> Effect (Maybe Element)
-documentGetElementById id = getDocument >>= getElementById id
-
-getTablatureTextArea :: Effect (Maybe HTMLTextAreaElement)
-getTablatureTextArea =
-  documentGetElementById "textareaTablature" <#>
-  (\maybeElement -> maybeElement >>= HTMLTextAreaElement.fromElement)
-
-getTablatureText :: Effect String
-getTablatureText = do
-  maybeTextArea <- getTablatureTextArea
+getTablatureEditorText :: forall output m . MonadEffect m => H.HalogenM State Action () output m String
+getTablatureEditorText = do
+  maybeTextArea <- getTablatureEditorElement
   case maybeTextArea of
-    Nothing -> Console.error "Could not find textareaTablature" *> pure ""
-    Just textArea -> HTMLTextAreaElement.value textArea 
+    Nothing -> H.liftEffect $ Console.error "Could not find textareaTablature" *> pure ""
+    Just textArea -> H.liftEffect $ WH.HTMLTextAreaElement.value textArea 
 
-setTablatureText :: String -> Effect Unit
-setTablatureText text = do
-  maybeTextArea <- getTablatureTextArea
+saveTablature :: forall output m . MonadEffect m => H.HalogenM State Action () output m Unit
+saveTablature = do
+  tablatureText <- getTablatureEditorText
+  saveTablatureToState tablatureText
+  saveTablatureToFragment tablatureText
+  where
+  saveTablatureToState tablatureText = do
+    state <- H.get
+    H.put { mode: state.mode, tablature: tablatureText }
+  saveTablatureToFragment tablatureText = do
+    case compressToEncodedURIComponent tablatureText of
+      Just compressed -> H.liftEffect $ setFragmentString compressed
+      Nothing -> H.liftEffect $ Console.error("Could not save tablature to URL")
+
+getTablatureTextFromFragment :: Effect String
+getTablatureTextFromFragment = do
+  fragment <- H.liftEffect getFragmentString
+  case decompressFromEncodedURIComponent fragment of
+    Just decompressed -> pure $ decompressed
+    Nothing ->  Console.error("Could not load tablature from URL") *> pure ""
+
+setTablatureEditorText :: forall output m . MonadEffect m => String -> H.HalogenM State Action () output m Unit
+setTablatureEditorText text = do
+  maybeTextArea <- getTablatureEditorElement
   case maybeTextArea of
-    Nothing -> Console.error "Could not find textareaTablature" *> pure unit
-    Just textArea -> HTMLTextAreaElement.setValue text textArea 
+    Nothing -> H.liftEffect $ Console.error "Could not find textareaTablature" *> pure unit
+    Just textArea -> H.liftEffect $ WH.HTMLTextAreaElement.setValue text textArea 
