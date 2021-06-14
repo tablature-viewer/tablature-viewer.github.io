4730642c3d87d71404c4e6c003bcbb8a6e5ca22b
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-06-14 23:55:03 +0200

    improve dozenal rendering

--- src/TablatureRenderer.purs
+++ src/TablatureRenderer.purs
@@ -6,11 +6,12 @@ import TablatureParser
 import Utils
 
 import Data.Array (fromFoldable)
-import Data.Either (Either(..))
-import Data.Int (Radix, decimal, radix, toStringAs)
-import Data.List (List(..), (:))
+import Data.Foldable (foldl)
+import Data.Int (decimal, radix, toStringAs)
+import Data.List (List(..), reverse, (:))
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.String (Pattern(..), Replacement(..), replace, replaceAll)
+import Data.String (Pattern(..), Replacement(..), replaceAll)
+import Data.String.Utils (repeat)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
@@ -27,22 +28,32 @@ renderTablatureDocument doc = map renderLine doc
   renderLine :: TablatureDocumentLine -> HH.HTML w i
   renderLine (CommentLine line) = HH.span [ classString "tabComment" ] [HH.text $ line <> "\n"]
   renderLine (TitleLine line) = HH.span_
-   [ HH.span [ classString "tabComment" ] [HH.text $ line.prefix]
-   , HH.span [ classString "tabTitle" ] [HH.text $ line.title]
-   , HH.span [ classString "tabComment" ] [HH.text $ line.suffix <> "\n"]
-   ]
-  renderLine (TablatureLine line) = HH.span_ $ fromFoldable $ map renderTablatureElem line <> HH.text "\n" : Nil
-  renderTablatureElem (Prefix string) = HH.span [ classString "tabPrefix" ] [ HH.text string ]
-  renderTablatureElem (Suffix string) = HH.span [ classString "tabSuffix" ] [ HH.text string ]
-  renderTablatureElem (Timeline string) = HH.span [ classString "tabTimeline" ] [ HH.text string ]
-  renderTablatureElem (Fret n) =
-    if dozenalString == "↊" || dozenalString == "↋"
-    then HH.span_ [ fretHtml, HH.span [ classString "tabTimeline" ] [ HH.text "-" ] ]
-    else fretHtml
-    where
-    fretHtml = HH.span [ classString "tabFret" ] [ HH.text $ dozenalString ]
-    dozenalString = toDozenalString n
-  renderTablatureElem (Special string) = HH.span [ classString "tabSpecial" ] [ HH.text string ]
+    [ HH.span [ classString "tabComment" ] [HH.text $ line.prefix]
+    , HH.span [ classString "tabTitle" ] [HH.text $ line.title]
+    , HH.span [ classString "tabComment" ] [HH.text $ line.suffix <> "\n"]
+    ]
+  -- Rendering elements needs care because the numbers ↊ and ↋ take less space than 10 and 11.
+  -- We need to make up for this with extra dashes at the first next Timeline element.
+  renderLine (TablatureLine line) = HH.span_ $ fromFoldable $ reverse $ result.acc <> HH.text "\n" : Nil
+    where result = foldl accTablatureElems {pendingDashes:0, acc: Nil} line
+  accTablatureElems :: { acc:: List (HH.HTML w i), pendingDashes:: Int } -> TablatureElem -> { acc:: List (HH.HTML w i), pendingDashes:: Int } 
+  accTablatureElems { pendingDashes, acc } elem = { pendingDashes: elemResult.pendingDashes, acc: (elemResult.result:acc) }
+    where elemResult = renderTablatureElem pendingDashes elem
+
+type TablatureElemRenderResult w i = { result:: HH.HTML w i, pendingDashes:: Int }
+
+renderTablatureElem :: forall w i. Int -> TablatureElem -> TablatureElemRenderResult w i
+renderTablatureElem pendingDashes (Prefix string) = { pendingDashes, result: HH.span [ classString "tabPrefix" ] [ HH.text string ] }
+renderTablatureElem pendingDashes (Suffix string) = { pendingDashes, result: HH.span [ classString "tabSuffix" ] [ HH.text string ] }
+renderTablatureElem pendingDashes (Special string) = { pendingDashes, result: HH.span [ classString "tabSpecial" ] [ HH.text string ] }
+renderTablatureElem pendingDashes (Timeline string) = { pendingDashes: 0, result: HH.span [ classString "tabTimeline" ] [ HH.text (fromMaybe "" (repeat pendingDashes "-") <> string) ] }
+renderTablatureElem pendingDashes (Fret n) =
+  if dozenalString == "↊" || dozenalString == "↋"
+  then { pendingDashes: pendingDashes + 1, result: fretHtml }
+  else { pendingDashes, result: fretHtml }
+  where
+  fretHtml = HH.span [ classString "tabFret" ] [ HH.text $ dozenalString ]
+  dozenalString = toDozenalString n
 
 toDozenalString :: Int -> String
 toDozenalString n = toStringAs dozenal n # replaceAll (Pattern "a") (Replacement "↊") # replaceAll (Pattern "b") (Replacement "↋")
