191fe56f6fd85df55f716ec943a2d6ad2836a6a3
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-06-20 12:43:52 +0200

    remove double url encoding

--- src/LocationString.js
+++ src/LocationString.js
@@ -9,10 +9,6 @@ exports.getLocationBaseString = function () {
 };
 
 exports.getFragmentString = function () {
-  return decodeURIComponent(exports.getRawFragmentString());
-};
-
-exports.getRawFragmentString = function () {
   var result = window.location.hash;
   if (result.startsWith("#"))
     return result.substring(1);
@@ -21,17 +17,11 @@ exports.getRawFragmentString = function () {
 
 exports.setFragmentString = function (value) {
   return function () {
-    setRawFragmentString(encodeURIComponent(value));
+    _setFragmentString(value);
   }
 };
 
-exports.setRawFragmentString = function (value) {
-  return function () {
-    setRawFragmentString(value);
-  }
-};
-
-var setRawFragmentString = function (value) {
+var _setFragmentString = function (value) {
   var newurl = window.location.protocol + "//" + window.location.host +
     window.location.pathname + window.location.search + '#' + value;
   // Only changing the fragment string shouldn't trigger a page reload in
@@ -41,20 +31,40 @@ var setRawFragmentString = function (value) {
   window.history.pushState({}, '', newurl);
 };
 
-var setRawQueryString = function (value) {
+
+exports.getQueryString = function () {
+  var result = window.location.search;
+  if (result.startsWith("?"))
+    return result.substring(1);
+  return "";
+};
+
+exports.setQueryString = function (value) {
+  return function () {
+    _setQueryString(value);
+  }
+};
+
+var _setQueryString = function (value) {
   var newurl = window.location.protocol + "//" + window.location.host +
-    window.location.pathname + '?' + value;
+    window.location.pathname + '?' + value + window.location.hash;
   // Set the location without triggering a page reload.
   window.history.pushState({}, '', newurl);
 };
 
-exports.setFragmentParameters = function (params) {
-  var encodedParams = Object.keys(params).map(function (key) {
-    return key + '=' + encodeURIComponent(params[key].replace('/', ''));
-  }).join('&');
 
-  exports.setRawFragmentString(encodedParams);
+/*
+exports.decodeAndGetFragmentString = function () {
+  return decodeURIComponent(exports.getFragmentString());
 };
+
+exports.encodeAndSetFragmentString = function (value) {
+  return function () {
+    _setFragmentString(encodeURIComponent(value));
+  }
+};
+*/
+
 
 // The popstate event of the Window interface is fired when the active history entry changes while the user navigates the session history. 
 // Hack for making sure hashchanges trigger a reload when nagivating history
@@ -64,12 +74,10 @@ window.addEventListener('popstate', () => {
 });
 
 // TODO: move to purescript
-// TODO: Also set scrollTop of main element to 0, because saving on a mobile phone still doesn't work properly.  
 var root = document.querySelector(':root');
 window.addEventListener('resize', () => {
   root.style.setProperty('--app-height', vh() + 'px');
 });
-
 function vh() {
   // - 1 because sometimes the innerHeight doesn't seem to be rounded correctly or something
   return Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 1;
--- src/LocationString.purs
+++ src/LocationString.purs
@@ -1,16 +1,12 @@
 module LocationString where
 
 import Prelude (Unit)
-
 import Effect (Effect)
-import Effect.Uncurried (EffectFn1)
-import Foreign.Object as Object
 
 -- TODO: we could probably just use Web.HTML.Location
 foreign import getLocationString :: Effect String
 foreign import getLocationBaseString :: Effect String
 foreign import getFragmentString :: Effect String
 foreign import setFragmentString :: String -> Effect Unit
-foreign import getRawFragmentString :: Effect String
-foreign import setRawFragmentString :: String -> Effect Unit
-foreign import setFragmentParameters :: EffectFn1 (Object.Object String) Unit
+foreign import getQueryString :: Effect String
+foreign import setQueryString :: String -> Effect Unit
