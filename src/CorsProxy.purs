8530a372af6f527aa0af2231e0941ad130fac64c
chtenb <ctenbrinke@gmail.com>
2023-03-11 10:23:45 +0100

    Fix proxy regression

--- src/CorsProxy.purs
+++ src/CorsProxy.purs
@@ -2,8 +2,8 @@ module CorsProxy where
 
 import Prelude
 
-import Affjax.Web as AX
 import Affjax.ResponseFormat as ResponseFormat
+import Affjax.Web as AX
 import AppState (Url)
 import Control.Monad.Maybe.Trans (MaybeT(..))
 import Data.Either (Either(..))
@@ -11,13 +11,19 @@ import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (liftEffect)
 import Effect.Console as Console
+import JSURI (encodeURIComponent)
 
 fetchThroughProxy :: forall m. MonadAff m => Url -> MaybeT m String
 fetchThroughProxy url = MaybeT do
-  let corsUrl = "https://api.codetabs.com/v1/proxy?quest=" <> url
-  response <- liftAff $ AX.get ResponseFormat.string corsUrl
-  liftEffect case response of
-    Left err -> do
-      Console.error $ "CORS Proxy response failed to decode: " <> AX.printError err
+  case encodeURIComponent url of
+    Nothing -> do
+      liftEffect $ Console.error $ "Could not encode " <> url
       pure Nothing
-    Right result -> pure $ Just result.body
+    Just encodedUrl -> do
+      let corsUrl = "https://api.codetabs.com/v1/proxy?quest=" <> encodedUrl
+      response <- liftAff $ AX.get ResponseFormat.string corsUrl
+      liftEffect case response of
+        Left err -> do
+          Console.error $ "CORS Proxy response failed to decode: " <> AX.printError err
+          pure Nothing
+        Right result -> pure $ Just result.body
--- test/AutoscrollSpec.purs
+++ test/AutoscrollSpec.purs
@@ -1,3 +1,4 @@
+module AutoscrollSpec where
 -- We want to store the autoscroll speed in the URL
 -- this means it has to be portable across devices
 -- So we need to store it in terms of lines per second and translate that to pixels per second when applying the autoscroll
