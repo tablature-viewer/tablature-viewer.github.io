22dc8677c5f29f7564ce636350f641383f02e6f1
chtenb <ctenbrinke@gmail.com>
2023-03-11 09:58:42 +0100

    upgrade

---
+++ test/AutoscrollSpec.purs
@@ -0,0 +1,4 @@
+-- We want to store the autoscroll speed in the URL
+-- this means it has to be portable across devices
+-- So we need to store it in terms of lines per second and translate that to pixels per second when applying the autoscroll
+-- pitfall: bookmarks don't get upated when you change the speed
